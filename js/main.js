17bb9b151fddb138b89c56556efd66ed1a681514
Alisher3344 <alisher3344ali@gmail.com>
2024-07-27 12:57:07 +0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,96 @@
+// // function1
+// function opwer(a, b) {
+//   console.log(Math.pow(a, b));
+// }
+// opwer(3, 2);
+
+// // function2
+// function mean(a, b) {
+//   console.log(2 / (a + b));
+//   console.log(Math.pow(a + b, 1));
+// }
+// mean(12, 48);
+
+// // function3
+// function sign(n) {
+//   if (n < 0) {
+//     n = -1;
+//   } else if (n == 0) {
+//     n = 0;
+//   } else {
+//     n = 1;
+//   }
+//   return n;
+// }
+// let a = sign(10);
+// console.log(a);
+
+// function4
+
+// // function5
+// function areaCircle(r) {
+//   let s = Math.PI * Math.pow(r, 2);
+//   return s;
+// }
+// let x = areaCircle(15);
+// console.log(x);
+
+// // function6
+// let x = 0;
+// function sumrRamnge(a, b) {
+//   for (let i = b; i <= a; i++) {
+//     x += i;
+//   }
+//   return x;
+// }
+// let y = sumrRamnge(10, 8);
+// console.log(y);
+
+// // function7
+// let x = 0;
+// function calc(a, b, s) {
+//   if (s == "+") {
+//     x = a + b;
+//   } else if (s == "-") {
+//     x = a - b;
+//   } else if (s == "*") {
+//     x = a * b;
+//   } else if (s == "/") {
+//     x = a / b;
+//   } else {
+//     x = 0;
+//   }
+//   return x;
+// }
+
+// let y = calc(2, 5, "+");
+// console.log(x);
+
+// // function8
+// function isEven(k) {
+//   if (k % 2 == 0) {
+//     y = 1;
+//   } else {
+//     y = 1.3;
+//   }
+//   return y;
+// }
+// isEven(2);
+// console.log(Number.isInteger(y));
+
+// // function9
+// function sort(a, b, c) {
+
+// }
+
+// // function10
+// let y = 0;
+// function isPower(k, n) {
+//   if (k % n == 0) {
+//     y = 1;
+//   } else y = 1.2;
+//   return y;
+// }
+// let x = isPower(10, 3);
+// console.log(Number.isSafeInteger(x));
+
